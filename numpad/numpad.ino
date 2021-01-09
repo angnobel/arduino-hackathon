b3332ff6c18c9efc9e7b03e2e7b94d5ff5a5f54f
angnobel <nobelang78@gmail.com>
2021-01-09 11:04:02 +0800

    remaining

--- numpad/numpad.ino
+++ numpad/numpad.ino
@@ -1,8 +1,13 @@
 #include <Keypad.h>
-const char first = 'C';
-const int second = '3';
-const int third = '8';
-const int fourth = '0';
+const char first = '1';
+const int second = '5';
+const int third = '2';
+const int fourth = '1';
+
+const int led1 = 10;
+const int led2 = 12;
+const int led3 = 11;
+const int led4 = 13;
 
 int correct = 0;
 int wrong = 0;
@@ -22,65 +27,133 @@ byte colPins[4] = {5, 4, 3, 2};
 
 Keypad customKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, 4, 4); 
 
+void solid () {
+  for (int i = 0; i < 3; i++) {
+      digitalWrite(led1, HIGH);
+      digitalWrite(led2, HIGH);
+      digitalWrite(led3, HIGH);
+      digitalWrite(led4, HIGH);
+      delay(200);
+      digitalWrite(led1, LOW);
+      digitalWrite(led2, LOW);
+      digitalWrite(led3, LOW);
+      digitalWrite(led4, LOW);
+      delay(200);
+  }
+
+  digitalWrite(led1, HIGH);   
+}
+
+void blinkN (int n) {
+  digitalWrite(led1, LOW);
+  digitalWrite(led2, LOW);
+  digitalWrite(led3, LOW);
+  digitalWrite(led4, LOW);
+  if (n == 0) {
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    digitalWrite(led3, HIGH);
+    digitalWrite(led4, HIGH);
+    delay(200);
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+    digitalWrite(led3, LOW);
+    digitalWrite(led4, LOW);
+    delay(200);
+  }
+  else if (n == 1) {
+    digitalWrite(led1, HIGH);
+  } else if (n == 2) {
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    
+  } else if (n == 3) {
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    digitalWrite(led3, HIGH);
+  } else if (n == 4) {
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    digitalWrite(led3, HIGH);
+    digitalWrite(led4, HIGH);
+  }
+}
+
 void setup(){
   Serial.begin(9600);
 }
   
 void loop(){
   char customKey = customKeypad.getKey();
-  
-  if (customKey){
-    Serial.println(customKey);
-    if (correct + wrong == 4) {
-      if (!complete) {
-        correct = 0;
-        wrong = 0;
-      }
-      
-    } else {
-      if (correct + wrong == 0) {
-        if (customKey == first) {
-          correct++;
-        } else {
-          wrong++;
-        }
-      } else if (correct + wrong == 1) {
-        if (customKey == second) {
-          correct++;
-        } else {
-          wrong++;
-        }
-      } else if (correct + wrong == 2) {
-        if (customKey == third) {
-          correct++;
-        } else {
-          wrong++;
-        }
-      } else if (correct + wrong == 3) {
-        if (customKey == fourth) {
-          correct++;
-        } else {
-          wrong++;
-        }
-
-        if (correct == 4) {
-          complete = true;
-          Serial.println("Completed");
-        } else {
-          Serial.print("Correct : ");
-          Serial.println(correct);
-          Serial.print("Wrong : ");
-          Serial.println(wrong);
-          correct = 0;
-          wrong = 0;
-        }
+  if (!complete) {
+      if (customKey){
+        Serial.println(customKey);
         
-
-        
-      }
-    }
+        if (correct + wrong == 4) {
+          if (!complete) {
+            correct = 0;
+            wrong = 0;
+          }
+          
+        } else {
+          if (correct + wrong == 0) {
+            if (customKey == first) {
+              correct++;
+            } else {
+              wrong++;
+            }
+          } else if (correct + wrong == 1) {
+            if (customKey == second) {
+              correct++;
+            } else {
+              wrong++;
+            }
+          } else if (correct + wrong == 2) {
+            if (customKey == third) {
+              correct++;
+            } else {
+              wrong++;
+            }
+          } else if (correct + wrong == 3) {
+            if (customKey == fourth) {
+              correct++;
+            } else {
+              wrong++;
+            }
     
-  }
+            if (correct == 4) {
+              complete = true;
+              Serial.println("Completed");
+            } else {
+              Serial.print("Correct : ");
+              Serial.println(correct);
+              Serial.print("Wrong : ");
+              Serial.println(wrong);
+              blinkN(correct);
+              correct = 0;
+              wrong = 0;
+              
+            }
+        
 
-  delay(100);
+        
+        }
+      }
+      delay(100);
+    }
+  } else {
+    digitalWrite(led1, HIGH);
+    delay(200);
+    digitalWrite(led1, LOW);
+    delay(200);
+
+//    if (customKey) {
+//      correct = 0;
+//      wrong = 0;
+//      complete = false;
+//    }
+  }
+  
+
+  
 }
--- wires/wires.ino
+++ wires/wires.ino
@@ -1,12 +1,23 @@
-const int red = 13;
-const int yellow = 12;
-const int white = 11;
-const int black = 10;
-const int led = 9;
+const int white = 13;
+const int green = 12;
+const int black = 11;
+const int yellow = 10;
+const int red = 9;
 
-// Sequence black, red, white, yellow
+const int red1 = 2;
+const int green1 = 3;
+const int red2 = 4;
+const int green2 = 5;
+const int red3 = 6;
+
+// Sequence Y,B,R,G,W
+const int first = black;
+const int second = green;
+const int third = red;
+const int fourth = yellow; 
+const int fifth = white;
+
 int testState = 0;
-boolean reset = false;
 int in = 0;
 int out = 1;
 
@@ -19,68 +30,93 @@ void setup() {
   pinMode(yellow, INPUT_PULLUP);
   pinMode(white, INPUT_PULLUP);
   pinMode(black, INPUT_PULLUP);
-  pinMode(led, OUTPUT);
+  pinMode(green, INPUT_PULLUP);
+  
+  pinMode(red1, OUTPUT);
+  pinMode(green1, OUTPUT);
+  pinMode(red2, OUTPUT);
+  pinMode(green2, OUTPUT);
+  pinMode(red3, OUTPUT);
 
 }
 
 void blink() {
-  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(red1, HIGH);
+  digitalWrite(red2, HIGH);
+  digitalWrite(red3, HIGH);
   delay(1000);                       // wait for a second
-  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(red1, LOW);
+  digitalWrite(red2, LOW);
+  digitalWrite(red3, LOW);
   delay(1000);                       // wait for a second
 }
 
-void solid() {
-  digitalWrite(led, HIGH);
+void flashGreen() {
+  digitalWrite(green1, HIGH);
+  digitalWrite(green2, HIGH);
+
+  delay(1000);                       // wait for a second
+  digitalWrite(green1, LOW);
+  digitalWrite(green2, LOW);
+  delay(1000); 
 }
 
 void loop() {
   // Get connection status
-  int redVal = digitalRead(red);
-  int yellowVal = digitalRead(yellow);
-  int whiteVal = digitalRead(white);
-  int blackVal = digitalRead(black);
+  int firstVal = digitalRead(first);
+  int secondVal = digitalRead(second);
+  int thirdVal = digitalRead(third);
+  int fourthVal = digitalRead(fourth);
+  int fifthVal = digitalRead(fifth);
   
   Serial.println(testState);
-//  Serial.println(redVal);
-//  Serial.println(yellowVal);
-//  Serial.println(whiteVal);
-//  Serial.println(blackVal);
     
   if (testState == -1) {
-    if (redVal == in && yellowVal == in && whiteVal ==in && blackVal == in) {
+    if (firstVal == in && secondVal == in && thirdVal ==in && fourthVal == in && fifthVal == in) {
       testState = 0;
     } else {
       // Blink
       blink();
     }
   } else if (testState == 0) {
-    if (redVal == in && yellowVal == in && whiteVal == in && blackVal == out) {
+    if (firstVal == out && secondVal == in && thirdVal == in && fourthVal == in && fifthVal == in) {
       testState = 1;
-    } else if( !(redVal == in && yellowVal == in && whiteVal ==in && blackVal == in)) {
+    } else if( !(firstVal == in && secondVal == in && thirdVal == in && fourthVal == in && fifthVal == in)) {
       testState = -1;
     }
   } else if (testState == 1) {
-    if (redVal == out && yellowVal == in && whiteVal == in && blackVal == out) {
+    if (firstVal == out && secondVal == out && thirdVal == in && fourthVal == in && fifthVal == in) {
       testState = 2;
-    } else if( !(redVal == in && yellowVal == in && whiteVal == in && blackVal == out)) {
+    } else if( !(firstVal == out && secondVal == in && thirdVal == in && fourthVal == in && fifthVal == in)) {
       testState = -1;
     }
   } else if (testState == 2) {
-    if (redVal == out && yellowVal == in && whiteVal == out && blackVal == out) {
+    if (firstVal == out && secondVal == out && thirdVal == out && fourthVal == in && fifthVal == in) {
       testState = 3;
-    } else if( !(redVal == out && yellowVal == in && whiteVal == in && blackVal == out)) {
+    } else if( !(firstVal == out && secondVal == out && thirdVal == in && fourthVal == in && fifthVal == in)) {
       testState = -1;
     }
   } else if (testState == 3) {
-    if (redVal == out && yellowVal == out && whiteVal == out && blackVal == out) {
+    if (firstVal == out && secondVal == out && thirdVal == out && fourthVal == out && fifthVal == in) {
       testState = 4;
-    } else if( !(redVal == out && yellowVal == in && whiteVal == out && blackVal == out)) {
+    } else if( !(firstVal == out && secondVal == out && thirdVal == out && fourthVal == in && fifthVal == in)) {
       testState = -1;
     }
   } else if (testState == 4) {
-    // Solid
-    solid();
+    if (firstVal == out && secondVal == out && thirdVal == out && fourthVal == out && fifthVal == out) {
+      testState = 5;
+    } else if( !(firstVal == out && secondVal == out && thirdVal == out && fourthVal == out && fifthVal == in)) {
+      testState = -1;
+    }
+  } else if (testState == 5) {
+    if (firstVal == in && secondVal == in && thirdVal ==in && fourthVal == in && fifthVal == in) {
+      testState == 0;
+      digitalWrite(green1, LOW);
+      digitalWrite(green2, LOW);
+    } else {
+      flashGreen();
+    }
+    
   }
   delay(400);
 }
